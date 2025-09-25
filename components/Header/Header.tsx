3548c57236a62414b89a98feaf5ceb0768b42abb
Nastia Komisar <anastasiakomissar0302@gmail.com>
2025-09-25 14:28:07 +0300

    Added header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,29 @@
+'use client';
+
 import React from "react";
+import Link from 'next/link';
+import { useState } from 'react';
+import css from './Header.module.css';
+import SideBar from "../SideBar/SideBar";
 
 const Header = () => {
-  return <header>Header</header>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+  <header className={css.header}>
+    <Link href="/" className={css.logo}>
+      <svg className={css.logoIcon}>
+        <use href="/leleka-sprite.svg#icon-logo" />
+      </svg>
+    </Link>
+    <button className={css.burgerBtn} onClick={() => setIsMenuOpen(true)}>
+      <svg className={css.burgerIcon}>
+        <use href="/leleka-sprite.svg#icon-menu-1" />
+      </svg>
+    </button>
+    {/* <SideBar isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)}></SideBar> */}
+  </header>
+  );
 };
 
 export default Header;
