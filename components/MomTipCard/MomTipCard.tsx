c4841870d99b5194d5a53a7159a220023ba3f29b
Word315 <boichuk315@gmail.com>
2025-09-26 16:51:58 +0200

    MomTipCard

--- components/MomTipCard/MomTipCard.tsx
+++ components/MomTipCard/MomTipCard.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import css from "./MomTipCard.module.css";
+import { getWeekDetails } from "@/lib/api/clientApi";
 
-const MomTipCard = () => {
-  return <div>MomTipCard</div>;
-};
+export default function MomTipCard({ weekNumber }: { weekNumber: number }) {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["momTip", weekNumber],
+    queryFn: () => getWeekDetails(weekNumber),
+  });
 
-export default MomTipCard;
+  if (isLoading) return <p>Завантаження...</p>;
+  if (error) return <p>Помилка при завантаженні поради</p>;
+
+  const todayIndex = new Date().getDay() === 0 ? 6 : new Date().getDay() - 1;
+  const tip = data?.momDailyTips?.[todayIndex];
+
+  return (
+    <section className={css.tipSection}>
+      <div className={css.tipHeader}>
+        <svg width={24} height={24} className={css.icon}>
+          <use href="/leleka-sprite.svg#icon-heart"></use>
+        </svg>
+        <h3 className={css.tipTitle}>Порада для мами</h3>
+      </div>
+      <p className={css.tipText}>{tip}</p>
+    </section>
+  );
+}
