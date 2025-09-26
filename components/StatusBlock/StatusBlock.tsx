e6a913d55bee269051318c65b4ef9a208f5acd98
Pavlo Markovskyi <pash199060@gmail.com>
2025-09-26 12:31:18 +0300

    add status-block

--- components/StatusBlock/StatusBlock.tsx
+++ components/StatusBlock/StatusBlock.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
+
+import css from "./StatusBlock.module.css";
+import { useAuthUserStore } from "@/lib/store/authStore";
+
+// const mockUser = {
+//   dueDate: "2026-04-04",
+// };
 
 const StatusBlock = () => {
-  return <div>StatusBlock</div>;
+  const user = useAuthUserStore((state) => state.user);
+  const dueDate = new Date(user.dueDate);
+  const isValidDate = !isNaN(dueDate.getTime());
+  if (!isValidDate) {
+    return (
+      <div className={css.wrapper}>
+        <div className={css.box}>
+          <p className={css.boxTitle}>Дата не задана</p>
+          <p className={css.boxTextNan}>Вкажіть дату пологів</p>
+        </div>
+      </div>
+    );
+  }
+
+  const today = new Date();
+  const pregancyStart = new Date(dueDate.getTime() - 280 * 24 * 60 * 60 * 1000);
+  const week = Math.floor(
+    (today.getTime() - pregancyStart.getTime()) / (7 * 24 * 60 * 60 * 1000)
+  );
+  const daysLeft = Math.ceil(
+    (dueDate.getTime() - today.getTime()) / (24 * 60 * 60 * 1000)
+  );
+
+  return (
+    <div className={css.wrapper}>
+      <div className={css.box}>
+        <p className={css.boxTitle}>Тиждень</p>
+        <p className={css.boxText}>{week}</p>
+      </div>
+      <div className={css.box}>
+        <p className={css.boxTitle}>Днів до зустрічі</p>
+        <p className={css.boxText}>~{daysLeft}</p>
+      </div>
+    </div>
+  );
 };
 
 export default StatusBlock;
