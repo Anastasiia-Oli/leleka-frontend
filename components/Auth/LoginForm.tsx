b6af7b7d15e88e7a242f960e2d8a3649a272a126
A_Oliinyk <aolejnik916@gmail.com>
2025-09-27 01:24:27 +0200

    add set user to login

--- components/Auth/LoginForm.tsx
+++ components/Auth/LoginForm.tsx
@@ -3,23 +3,27 @@
 import axios from "axios";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import { login } from "@/lib/api/clientApi"; 
+import { login } from "@/lib/api/clientApi";
 import { useRouter } from "next/navigation";
 import { toast } from "react-hot-toast";
 import styles from "./AuthPage.module.css";
 import Link from "next/link";
+import { useAuthUserStore } from "@/lib/store/authStore";
 
 type FormValues = { email: string; password: string };
 
 const Schema = Yup.object({
   email: Yup.string().email("Некоректний email").required("Вкажіть email"),
-  password: Yup.string().min(6, "Мінімум 6 символів").required("Вкажіть пароль"),
+  password: Yup.string()
+    .min(6, "Мінімум 6 символів")
+    .required("Вкажіть пароль"),
 });
 
 const AFTER_LOGIN = "/";
 
 export default function LoginForm() {
   const router = useRouter();
+  const setUser = useAuthUserStore((state) => state.setUser);
 
   return (
     <Formik<FormValues>
@@ -31,15 +35,26 @@ export default function LoginForm() {
           password: values.password,
         };
         try {
-          await login(payload); 
+          const res = await login(payload);
+          const user = res.data;
+
+          if (user) {
+            setUser(user);
+            router.push("/profile");
+            console.log("vce horosho");
+          } else {
+            console.log("problemaaa");
+          }
+
           toast.success("Вхід успішний!");
           router.push(AFTER_LOGIN);
         } catch (err: unknown) {
           const msg = axios.isAxiosError(err)
-            ? (err.response?.data as { message?: string })?.message || "Помилка входу"
+            ? (err.response?.data as { message?: string })?.message ||
+              "Помилка входу"
             : err instanceof Error
-            ? err.message
-            : "Помилка входу";
+              ? err.message
+              : "Помилка входу";
           toast.error(msg);
         } finally {
           setSubmitting(false);
@@ -53,7 +68,11 @@ export default function LoginForm() {
         return (
           <Form>
             <label className={styles.label}>
-              <span className={`${styles.labelText} ${emailHasError ? styles.labelTextError : ""}`}>Пошта*</span>
+              <span
+                className={`${styles.labelText} ${emailHasError ? styles.labelTextError : ""}`}
+              >
+                Пошта*
+              </span>
               <Field
                 type="email"
                 name="email"
@@ -63,11 +82,19 @@ export default function LoginForm() {
                 autoComplete="email"
                 inputMode="email"
               />
-              <ErrorMessage name="email" component="div" className={styles.error} />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className={styles.error}
+              />
             </label>
 
             <label className={styles.label}>
-              <span className={`${styles.labelText} ${passwordHasError ? styles.labelTextError : ""}`}>Пароль*</span>
+              <span
+                className={`${styles.labelText} ${passwordHasError ? styles.labelTextError : ""}`}
+              >
+                Пароль*
+              </span>
               <Field
                 type="password"
                 name="password"
@@ -76,15 +103,26 @@ export default function LoginForm() {
                 aria-invalid={passwordHasError}
                 autoComplete="current-password"
               />
-              <ErrorMessage name="password" component="div" className={styles.error} />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className={styles.error}
+              />
             </label>
 
-            <button type="submit" disabled={isSubmitting} className={styles.btnPink}>
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className={styles.btnPink}
+            >
               Увійти
             </button>
 
             <p className={styles.helper}>
-              Немає акаунта? <Link href="/auth/register" className={styles.helperLink}>Зареєструватись</Link>
+              Немає акаунта?{" "}
+              <Link href="/auth/register" className={styles.helperLink}>
+                Зареєструватись
+              </Link>
             </p>
           </Form>
         );
