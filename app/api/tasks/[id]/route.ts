82da3ff9d860ff972df7a42f1334d045d4a94613
Dmytro Teren <leo20091986@gmail.com>
2025-09-26 18:59:37 +0300

    add routes

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -1,16 +1,35 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { api } from "../../api";
+import { NextRequest, NextResponse } from "next/server";
+import { api } from "../../api";
+import { cookies } from "next/headers";
+import { isAxiosError } from "axios";
+import { logErrorResponse } from "../../_utils/utils";
 
-// type Props = {
-//   params: Promise<{ id: string }>;
-// };
+type Props = {
+  params: Promise<{ id: string }>;
+};
 
-// export async function PATCH({ params }: Props, request: NextRequest) {
-//   try {
-//     const { id } = await params;
-//     const res = await api(`/tasks/${id}`, {});
-//     return NextResponse.json(res.data, { status: res.status });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+export async function PATCH({ params }: Props, request: NextRequest) {
+  try {
+    const cookieStore = await cookies();
+    const { id } = await params;
+    const body = await request.json();
+
+    const res = await api.patch(`/tasks/${id}`, body, {
+      headers: { Cookie: cookieStore.toString() },
+    });
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
+    }
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
