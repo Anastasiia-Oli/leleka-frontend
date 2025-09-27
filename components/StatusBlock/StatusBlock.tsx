b5ef57b551dd7a838e7e8aa8dd390f61c92c93ae
Pavlo Markovskyi <pash199060@gmail.com>
2025-09-27 11:11:14 +0300

    fx date

--- components/StatusBlock/StatusBlock.tsx
+++ components/StatusBlock/StatusBlock.tsx
@@ -3,14 +3,10 @@
 import css from "./StatusBlock.module.css";
 import { useAuthUserStore } from "@/lib/store/authStore";
 
-// const mockUser = {
-//   dueDate: "2026-04-04",
-// };
-
 const StatusBlock = () => {
   const user = useAuthUserStore((state) => state.user);
-  const dueDate = new Date(user.dueDate);
-  const isValidDate = !isNaN(dueDate.getTime());
+  const dueDate = new Date(user.dueDate ? user.dueDate + "T00:00:00" : "");
+  const isValidDate = dueDate instanceof Date && !isNaN(dueDate.getTime());
   if (!isValidDate) {
     return (
       <div className={css.wrapper}>
