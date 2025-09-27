acdfc0a9ecbdea9649c74a4d1a28a11378423e8b
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-09-28 01:42:13 +0300

    0142 2809

--- app/api/diaries/route.ts
+++ app/api/diaries/route.ts
@@ -7,7 +7,7 @@ import { logErrorResponse } from '../_utils/utils';
 
 export async function GET(request: NextRequest) {
     try {
-
+        const cookieStore = await cookies();
         const res = await api('/api/diaries', {
             headers: {
                 Cookie: cookieStore.toString(),
