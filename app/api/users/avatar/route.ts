e71716cc87ce95876abd94d03c0d86bdbda77bf8
Yelyzaveta Shevchenko <lizkasheva26@gmail.com>
2025-09-28 17:49:59 +0200

    Update route.ts

--- app/api/users/avatar/route.ts
+++ app/api/users/avatar/route.ts
@@ -7,10 +7,10 @@ export async function PATCH(req: Request) {
   try {
     const cookieStore = await cookies();
     const formData = await req.formData();
-    const file = formData.get("avatar") as File;
+    const file = formData.get("photo") as File;
 
     const backendFormData = new FormData();
-    backendFormData.append("avatar", Buffer.from(await file.arrayBuffer()), file.name);
+    backendFormData.append("photo", Buffer.from(await file.arrayBuffer()), file.name);
 
     const { data } = await api.patch("api/users/avatar", backendFormData, {
       headers: {
