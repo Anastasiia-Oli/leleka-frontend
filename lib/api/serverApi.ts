12302873e015ffcf53cd5d26a8a4952bd31e2ede
Mariia Zhuk <mariia.zhuk1023@gmail.com>
2025-09-29 05:06:53 +0300

    delete functions

--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -2,8 +2,6 @@ import { cookies } from "next/headers";
 import nextServer from "./api";
 import { User } from "@/types/user";
 
-import { DiaryEntryData } from "./clientApi";
-
 export const checkServerSession = async () => {
   const cookieStore = await cookies();
   const res = await nextServer.post("/auth/refresh", {
@@ -22,41 +20,6 @@ export const getMeServer = async () => {
     },
   });
   return response.data;
-};
-
-export const createDiaryEntryServer = async (data: DiaryEntryData) => {
-  const cookieStore = await cookies();
-  const res = await nextServer.post("/diaries", data, {
-    headers: { Cookie: cookieStore.toString() },
-  });
-  return res.data;
-};
-
-export const updateDiaryEntryServer = async (
-  id: string,
-  data: DiaryEntryData
-) => {
-  const cookieStore = await cookies();
-  const res = await nextServer.patch(`/diaries/${id}`, data, {
-    headers: { Cookie: cookieStore.toString() },
-  });
-  return res.data;
-};
-
-export const getDiaryEntriesServer = async () => {
-  const cookieStore = await cookies();
-  const res = await nextServer.get("/diaries", {
-    headers: { Cookie: cookieStore.toString() },
-  });
-  return res.data;
-};
-
-export const getDiaryEntryByIdServer = async (id: string) => {
-  const cookieStore = await cookies();
-  const res = await nextServer.get(`/diaries/${id}`, {
-    headers: { Cookie: cookieStore.toString() },
-  });
-  return res.data;
 };
 
 export const getEmotionsServer = async () => {
