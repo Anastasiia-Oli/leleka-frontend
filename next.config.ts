b154611af882ea33e012c5a538f45335ee4599c7
Anastasiia Oliinyk <aolejnik916@gmail.com>
2025-09-29 14:49:38 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/proxy/:path*',
-        destination: 'https://leleka-backend-1.onrender.com/:path*'
-      }
-    ]
-  }
-}
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
