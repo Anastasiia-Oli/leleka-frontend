e597455c31c967e03e2eff63e1b953d7e20974c7
Shcherbyna Oleksandr <Scherbinaalexandr75@gmail.com>
2025-09-30 09:20:43 +0300

    2 commit before pull

--- components/Breadcrumbs/Breadcrumbs.tsx
+++ components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,11 +1,11 @@
-"use client";
+// "use client";
 
-import React from "react";
+// import React from "react";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { ukrainianTranslations } from '@/lib/translations/translations';
-// Імпортуйте ваші CSS-модулі
-import styles from './Breadcrumbs.module.css'// Приклад імпорту для CSS-модулів
+// import Link from 'next/link';
+// import { usePathname } from 'next/navigation';
+// import { ukrainianTranslations } from '@/lib/translations/translations';
+// // Імпортуйте ваші CSS-модулі
+// import styles from './Breadcrumbs.module.css'// Приклад імпорту для CSS-модулів
 
-export default Breadcrumbs;
+// export default Breadcrumbs;
