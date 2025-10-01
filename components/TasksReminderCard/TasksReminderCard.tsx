744ede8603086ba6cb0b88caa1f78d3b967347fb
A_Oliinyk <aolejnik916@gmail.com>
2025-10-01 17:14:20 +0200

    add modal open logic to button +

--- components/TasksReminderCard/TasksReminderCard.tsx
+++ components/TasksReminderCard/TasksReminderCard.tsx
@@ -71,7 +71,7 @@ const TasksReminderCard = () => {
     <div className={css.card}>
       <div className={css.listHeader}>
         <h2 className={css.title}>Важливі завдання</h2>
-        <button className={css.newTask}>
+        <button className={css.newTask} onClick={handleOpenModal}>
           <svg viewBox="0 0 32 32" width="20" height="20">
             <path
               d="M16 03v26M03 16h26"
