0ab4cead96ee99f0c19223373f2f419ddf888225
Mariia Zhuk <mariia.zhuk1023@gmail.com>
2025-10-01 13:40:33 +0300

    added opening of the mod with my day

--- components/FeelingCheckCard/FeelingCheckCard.tsx
+++ components/FeelingCheckCard/FeelingCheckCard.tsx
@@ -1,15 +1,32 @@
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
+import AddDiaryEntryModal from "../Diary/AddDiaryEntryModal/AddDiaryEntryModal";
 import css from "./FeelingCheckCard.module.css";
 
 const FeelingCheckCard = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div className={css.card}>
       <h2>Як ви себе почуваєте?</h2>
       <h3>Рекомендація на сьогодні:</h3>
       <p> Занотуйте незвичні відчуття у тілі.</p>
-      <Link href={"/diary"} className={css.btn}>
+
+      {/* замінила Link на кнопку */}
+      <button
+        type="button"
+        className={css.btn}
+        onClick={() => setIsModalOpen(true)}
+      >
         Зробити запис у щоденник
-      </Link>
+      </button>
+
+      <AddDiaryEntryModal
+        isOpen={isModalOpen}
+        mode="create"
+        onClose={() => setIsModalOpen(false)}
+      />
     </div>
   );
 };
